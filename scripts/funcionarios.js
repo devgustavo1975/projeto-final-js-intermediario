6a435c6be6b3cd4af6fbc3569eee940cb25d9f3b
Gustavo Fernandes de Almeida <gustty75rj@hotmail.com>
2025-07-10 10:49:55 -0300

    Add files via upload

---
+++ scripts/funcionarios.js
@@ -0,0 +1,59 @@
+const pessoas = [
+    {
+        Nome: "Maria da Silva",
+        Dt_Nascimento: "1990-03-29",
+        Sexo: "Feminino",
+        grauEscolaridade: "Ensino Superior Completo",
+        endereco: "Rua das Flores, 123 - São Paulo, SP",
+        foto: "https://randomuser.me/api/portraits/women/44.jpg"
+    },
+    {
+        Nome: "João Souza",
+        Dt_Nascimento: "1985-10-22",
+        Sexo: "Masculino",
+        grauEscolaridade: "Ensino Médio Completo",
+        endereco: "Avenida Bruxelas, 350 - Rio de Janeiro, RJ",
+        foto: "https://randomuser.me/api/portraits/women/44.jpg"
+    },
+    {
+        Nome: "João Pereira Lima",
+        Dt_Nascimento: "1985-07-15",
+        Sexo: "Masculino",
+        grauEscolaridade: "Ensino Médio Completo",
+        endereco: "Avenida Brasil, 456 - Rio de Janeiro, RJ",
+        foto: "https://randomuser.me/api/portraits/men/12.jpg"
+    },
+    {
+        Nome: "Ana Beatriz Souza",
+        Dt_Nascimento: "1992-11-23",
+        Sexo: "Feminino",
+        grauEscolaridade: "Ensino Superior Incompleto",
+        endereco: "Rua das Acácias, 89 - Belo Horizonte, MG",
+        foto: "https://randomuser.me/api/portraits/women/18.jpg"
+    },
+    {
+        Nome: "Carlos Eduardo Ramos",
+        Dt_Nascimento: "1979-04-02",
+        Sexo: "Masculino",
+        grauEscolaridade: "Ensino Superior Completo",
+        endereco: "Rua das Laranjeiras, 321 - Curitiba, PR",
+        foto: "https://randomuser.me/api/portraits/men/25.jpg"
+    },
+    {
+        Nome: "Fernanda Lima Rocha",
+        Dt_Nascimento: "1988-09-12",
+        Sexo: "Feminino",
+        grauEscolaridade: "Pós-graduação",
+        endereco: "Rua João Pessoa, 111 - Salvador, BA",
+        foto: "https://randomuser.me/api/portraits/women/39.jpg"
+    },
+    {
+        Nome: "Ricardo Martins Alves",
+        Dt_Nascimento: "1995-01-08",
+        Sexo: "Masculino",
+        grauEscolaridade: "Ensino Técnico",
+        endereco: "Avenida Independência, 567 - Porto Alegre, RS",
+        foto: "https://randomuser.me/api/portraits/men/33.jpg"
+    }
+
+];
