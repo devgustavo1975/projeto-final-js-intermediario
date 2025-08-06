2e1dcd9bae0c2b2d7dbf4707a7d235b99d40d40b
Gustavo Fernandes de Almeida <gustty75rj@hotmail.com>
2025-08-06 18:45:26 -0300

    Update lista.js

--- scripts/lista.js
+++ scripts/lista.js
@@ -1,14 +1,14 @@
  import { pessoas } from './funcionarios.js';
 
-    const container = document.getElementById('listaFuncionarios');
+const container = document.getElementById('listaFuncionarios');
 
-    pessoas.forEach(pessoa => {
-      const card = document.createElement('div');
-      card.className = 'col-md-4 mb-4';
-      card.innerHTML = `
-        <div class="card h-100 shadow">
+pessoas.forEach(pessoa, index => {
+  const card = document.createElement('div');
+  card.className = 'col-md-4 mb-4';
+  card.innerHTML = `
+        <div class="card h-100 shadow" id="${index + 1}">
           <img src="${pessoa.foto}" class="card-img-top" alt="${pessoa.nome}">
-          <div class="card-body">
+          <div class="card-body link-card">
             <h5 class="card-title">${pessoa.nome}</h5>
             <p class="card-text"><strong>Data de Nascimento:</strong> ${new Date(pessoa.dtNascimento).toLocaleDateString('pt-BR')}</p>
             <p class="card-text"><strong>Sexo:</strong> ${pessoa.sexo}</p>
@@ -17,5 +17,11 @@
           </div>
         </div>
       `;
-      container.appendChild(card);
-    });
+  container.appendChild(card);
+});
+
+container.addEventListener('click', function (event) {
+  const card = event.target.closest('.card');
+  console.log(card.id.toString());
+  //window.location.href = `atualizarFuncionario.html/${card.id}`;
+});
