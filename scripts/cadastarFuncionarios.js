5c9fc1fc8b42b37de6f18b17c95880d63190d1e9
Gustavo Fernandes de Almeida <gustty75rj@hotmail.com>
2025-10-08 20:36:39 -0300

    Add files via upload

--- scripts/cadastarFuncionarios.js
+++ scripts/cadastarFuncionarios.js
@@ -5,68 +5,45 @@ function cadastrarFuncionarios() {
     let sexo = document.getElementById('sexo').value
     let grauEscolaridade = document.getElementById('grauEscolaridade').value
     let endereco = document.getElementById('endereco').value
-    let optouVT = document.getElementById('optouVT').value
-    let salarioAtual = document.getElementById('salarioAtual').value
+    const optouVTRadio = document.querySelector('input[name="optouVT"]:checked');
+    const optouVT = optouVTRadio ? optouVTRadio.value === 'true' : false;
     let valorPassagem = document.getElementById('valorPassagem').value
+    let cargo = document.getElementById('cargo').value
+    let salario = Number(document.getElementById('salario').value)
+    let dataInicio = document.getElementById('dataInicio').value
+    let dataDemissao = document.getElementById('dataDemissao').value
 
-    // REGEX
-    const regexNome = /^[A-Za-zÀ-ÿ\s]+$/
-    const regexCargo = /^[A-Za-zÀ-ÿ0-9\s]+$/
+    valorPassagem = parseFloat(valorPassagem);
 
-    // VALIDAÇÃO
-    if(!nome || !regexNome.test(nome)){
-        alert('FAVOR PREENCHER O CAMPO NOME COM LETRAS APENAS!')
-        return
-    }
+    // if (nome == '' || nome == null || nome == undefined) {
+    //     alert('FAVOR PREENCHER O CAMPO NOME!')
+    // }
+    // if (sobrenome == '' || sobrenome == null || sobrenome == undefined) {
+    //     alert('FAVOR PREENCHER O CAMPO SOBRENOME!')
+    // }
+    // if (dtNascimento == '' || dtNascimento == null || dtNascimento == undefined) {
+    //     alert('FAVOR PREENCHER O CAMPO DATA DE NASCIMENTO!')
+    // }
+    // if (sexo == '' || sexo == null || sexo == undefined) {
+    //     alert('FAVOR PREENCHER O CAMPO SEXO!')
+    // }
+    // if (grauEscolaridade == '' || grauEscolaridade == null || grauEscolaridade == undefined) {
+    //     alert('FAVOR PREENCHER O CAMPO GRAU DE ESCOLARIDADE!')
+    // }
+    // if (endereco == '' || endereco == null || endereco == undefined) {
+    //     alert('FAVOR PREENCHER O CAMPO ENDEREÇO!')
+    // }
+    // if (optouVT == '' || optouVT == null || optouVT == undefined) {
+    //     alert('FAVOR PREENCHER O CAMPO OPTOU VT!')
+    // }
 
-    if(!sobrenome || !regexNome.test(sobrenome)){
-        alert('FAVOR PREENCHER O CAMPO SOBRENOME COM LETRAS APENAS!')
-        return
-    }
 
-    if(!dtNascimento){
-        alert('FAVOR PREENCHER O CAMPO DATA DE NASCIMENTO!')
-        return
-    }
+    // if (isNaN(valorPassagem)) {
+    //     alert('FAVOR PREENCHER O CAMPO VALOR DA PASSAGEM COM UM NÚMERO VÁLIDO!');
+    //     return;
+    // }
+    console.log(optouVT)
 
-    if(!sexo){
-        alert('FAVOR PREENCHER O CAMPO SEXO!')
-        return
-    }
-
-    if(!grauEscolaridade){
-        alert('FAVOR PREENCHER O CAMPO GRAU DE ESCOLARIDADE!')
-        return
-    }
-
-    if(!endereco){
-        alert('FAVOR PREENCHER O CAMPO ENDEREÇO!')
-        return
-    }
-
-    if(!optouVT){
-        alert('FAVOR PREENCHER O CAMPO OPTOU VT!')
-        return
-    }
-
-    if(!salarioAtual){
-        alert('FAVOR PREENCHER O CAMPO SALÁRIO ATUAL!')
-        return
-    }
-
-    if(!valorPassagem){
-        alert('FAVOR PREENCHER O CAMPO VALOR DA PASSAGEM!')
-        return
-    }
-
-    // Exemplo de cargo vindo de outro campo ou digitado
-    let cargo = "Desenvolvedora Senior"
-    if(!regexCargo.test(cargo)){
-        alert('O CAMPO CARGO DEVE CONTER APENAS LETRAS E NÚMEROS!')
-        return
-    }
-
-    // Envio dos dados
     fetch('https://node-vercel-app-rho.vercel.app/api/funcionarios', {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -79,23 +56,18 @@ function cadastrarFuncionarios() {
                 "grauEscolaridade": `${grauEscolaridade}`,
                 "endereco": `${endereco}`,
                 "foto": "foto",
-                "salarioAtual": salarioAtual,
                 "valorPassagem": valorPassagem,
                 "optouVT": optouVT,
-                "historicoCargosESalarios": [
-                    {
-                        "cargo": cargo,
-                        "salario": 5000,
-                        "dataInicio": "2021-01-01",
-                        "dataFim": "null"
-                    }
-                ]
+                "cargo": cargo,
+                "salario": salario,
+                "dataInicio": dataInicio,
+                "dataDemissao": dataDemissao
+
             }
         )
     })
-    .then(resp => resp.json())
-    .then(dados => console.log(dados))
-    .catch(err => console.error("Erro na requisição:", err));
+        .then(resp => resp.json())
+        .then(dados => console.log(dados))
+        .catch(err => console.error("Erro na requisição:", err));
 }
-
 
